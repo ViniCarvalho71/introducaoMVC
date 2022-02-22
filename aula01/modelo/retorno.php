4f757f76efd6fac0b90317c56cf527660e515571
ViniCarvalho71 <carvalhovinicius71@gmail.com>
2022-02-22 10:49:58 -0300

    commit

--- aula01/modelo/retorno.php
+++ aula01/modelo/retorno.php
@@ -6,25 +6,25 @@ $ANIMAL = $_REQUEST['animal'];
 
 
 if(empty($NOME)){
-    $dado = array(
+    $dados = array(
         "tipo" => 'error',
         "mesagem" => 'Existem campos obrigatórios não preechidos'
     );
 } else {
     switch($ANIMAL){
         case '1' : $dados = array(
-         "tipo" => "cachorro.jpg",
-         "mensagem" => "Olá, $NOME, seu animal preferido é o cachorro"
+            "tipo" => "cachorro.jpg",
+            "mensagem" => "Olá, $NOME, seu animal preferido é o cachorro"
         );
         break;
         case '2' : $dados = array(
-         "tipo" => "gato.jpg",
-         "mensagem" => "Olá, $NOME, seu animal preferido é o gato"
+            "tipo" => "gato.jpg",
+            "mensagem" => "Olá, $NOME, seu animal preferido é o gato"
         );
         break;
         case '3' : $dados = array(
-          "tipo" => "peixe.jpg",
-          "mensagem" => "Olá, $NOME, seu animal preferido é o peixe"
+            "tipo" => "peixe.jpg",
+            "mensagem" => "Olá, $NOME, seu animal preferido é o peixe"
         );
         break;
     }
